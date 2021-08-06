0f825349a93dc017dbe8ed3feb3e1a2a7e735896
minerdev <minerdev2020@gmail.com>
2021-08-06 16:56:45 +0900

    style: 모집글 아이템 디자인 변경 등
    - 모집글 아이템 디자인 변경
    - 모집글 아이템 간 구분선 추가
    - 모집글 화면 상단 툴바의 작성 버튼 아이콘 변경

--- app/src/main/java/com/minerdev/exermate/view/fragment/GatheringFragment.kt
+++ app/src/main/java/com/minerdev/exermate/view/fragment/GatheringFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.minerdev.exermate.R
 import com.minerdev.exermate.adapter.GatheringAdapter
@@ -21,6 +22,12 @@ class GatheringFragment : Fragment() {
     ): View {
         val manager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         binding.recyclerView.layoutManager = manager
+        binding.recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                manager.orientation
+            )
+        )
         binding.recyclerView.adapter = adapter
 
         adapter.clickListener = object : GatheringAdapter.OnItemClickListener {
@@ -35,18 +42,66 @@ class GatheringFragment : Fragment() {
 
         adapter.submitList(
             listOf(
-                Gathering(id = 1, createdAt = "21.06.30", title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"),
-                Gathering(id = 2, createdAt = "21.06.30", title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"),
-                Gathering(id = 3, createdAt = "21.06.30", title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"),
-                Gathering(id = 4, createdAt = "21.06.30", title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"),
-                Gathering(id = 5, createdAt = "21.06.30", title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"),
-                Gathering(id = 6, createdAt = "21.06.30", title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"),
-                Gathering(id = 7, createdAt = "21.06.30", title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"),
-                Gathering(id = 8, createdAt = "21.06.30", title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"),
-                Gathering(id = 9, createdAt = "21.06.30", title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"),
-                Gathering(id = 10, createdAt = "21.06.30", title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"),
-                Gathering(id = 11, createdAt = "21.06.30", title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"),
-                Gathering(id = 12, createdAt = "21.06.30", title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유")
+                Gathering(
+                    id = 1,
+                    createdAt = "21.06.30",
+                    title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"
+                ),
+                Gathering(
+                    id = 2,
+                    createdAt = "21.06.30",
+                    title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"
+                ),
+                Gathering(
+                    id = 3,
+                    createdAt = "21.06.30",
+                    title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"
+                ),
+                Gathering(
+                    id = 4,
+                    createdAt = "21.06.30",
+                    title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"
+                ),
+                Gathering(
+                    id = 5,
+                    createdAt = "21.06.30",
+                    title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"
+                ),
+                Gathering(
+                    id = 6,
+                    createdAt = "21.06.30",
+                    title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"
+                ),
+                Gathering(
+                    id = 7,
+                    createdAt = "21.06.30",
+                    title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"
+                ),
+                Gathering(
+                    id = 8,
+                    createdAt = "21.06.30",
+                    title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"
+                ),
+                Gathering(
+                    id = 9,
+                    createdAt = "21.06.30",
+                    title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"
+                ),
+                Gathering(
+                    id = 10,
+                    createdAt = "21.06.30",
+                    title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"
+                ),
+                Gathering(
+                    id = 11,
+                    createdAt = "21.06.30",
+                    title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"
+                ),
+                Gathering(
+                    id = 12,
+                    createdAt = "21.06.30",
+                    title = "한국 여자 배구 팀의 도쿄 올림픽 경기를 보고 전세계가 극찬을 아끼지 않은 이유"
+                )
             )
         )
 
